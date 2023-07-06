9231412ddb8b53639b2e93485328e3e11f7ab6f2
N3v3R3nD <henning.midtbo@gmail.com>
2023-07-07 01:06:53 +0200

    Add files via upload

---
+++ db_operations.py
@@ -0,0 +1,76 @@
+import psycopg2
+from datetime import datetime, timedelta
+import logging
+
+def connect_to_db():
+    # Connect to the database
+    logging.info('Connecting to the database')
+    conn = psycopg2.connect(
+        host='localhost',
+        database='stock',
+        user='postgres',
+        password='test123'
+    )
+    return conn
+
+def create_tables(cur):
+    # Create forecast_data table if it doesn't exist
+    logging.info('Creating forecast_data table if it doesn\'t exist')
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS forecast_data (
+            date DATE PRIMARY KEY,
+            forecast FLOAT
+        )
+    """)
+
+    # Create loss_data table if it doesn't exist
+    logging.info('Creating loss_data table if it doesn\'t exist')
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS loss_data (
+            epoch INTEGER PRIMARY KEY,
+            loss FLOAT
+        )
+    """)
+
+    # Create actual_vs_predicted table if it doesn't exist
+    logging.info('Creating actual_vs_predicted table if it does not exist')
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS actual_vs_predicted (
+            date DATE PRIMARY KEY,
+            actual_price FLOAT,
+            predicted_price FLOAT
+        )
+    """)
+
+
+def insert_data(cur, history, Y_train, train_predict, test_predict):
+    # Insert forecast data into the database
+    logging.info('Inserting forecast data into the database')
+    for i in range(len(test_predict)):
+        date = (datetime.today() + timedelta(days=i)).strftime('%Y-%m-%d')
+        forecast = test_predict[i][0]
+        cur.execute(f"INSERT INTO forecast_data (date, forecast) VALUES ('{date}', {forecast}) ON CONFLICT (date) DO UPDATE SET forecast = {forecast}")
+
+    # Insert loss data into the database
+    logging.info('Inserting loss data into the database')
+    for i, loss in enumerate(history['loss']):
+        cur.execute(f"INSERT INTO loss_data (epoch, loss) VALUES ({i}, {loss}) ON CONFLICT (epoch) DO UPDATE SET loss = {loss}")
+
+    # Insert actual and predicted prices into the database
+    logging.info('Inserting actual and predicted prices into the database')
+    for i in range(len(Y_train)):
+        date = (datetime.today() - timedelta(days=len(Y_train)-i))
+        date_str = date.strftime('%Y-%m-%d')
+        actual_price = Y_train[i][0]
+        predicted_price = train_predict[i][0]
+        cur.execute(f"""
+            INSERT INTO actual_vs_predicted (date, actual_price, predicted_price) 
+            VALUES ('{date_str}', {actual_price}, {predicted_price}) 
+            ON CONFLICT (date) DO UPDATE 
+            SET actual_price = {actual_price}, predicted_price = {predicted_price}
+        """)
+
+def close_connection(conn):
+    # Commit changes and close connection
+    conn.commit()
+    conn.close()
---
+++ next1.py
@@ -0,0 +1,344 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import yfinance as yf
+import psycopg2
+import numpy as np
+import pandas as pd
+import logging
+import json
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dropout
+from sklearn.model_selection import TimeSeriesSplit, RandomizedSearchCV, train_test_split
+from tensorflow.keras import regularizers
+from scikeras.wrappers import KerasRegressor
+from datetime import datetime, timedelta
+import time
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
+import pandas_datareader as pdr
+from sklearn.model_selection import KFold
+from pandas.tseries.holiday import USFederalHolidayCalendar
+import db_operations
+# Set up logging
+logging.basicConfig(filename='next1.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Set the format for console output
+console_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+console_handler.setFormatter(console_formatter)
+
+# Add the console handler to the logger
+logging.getLogger('').addHandler(console_handler)
+
+logging.info('Starting script')
+
+# Fetch data using yfinance
+logging.info('Fetching data using yfinance')
+today = datetime.today().strftime('%Y-%m-%d')
+data = yf.download('SPY', start='2021-01-29', end=today)
+logging.info('Data downloaded from Yahoo Finance')
+logging.info(f'Data shape: {data.shape}')
+logging.info('First few rows of the data:')
+
+# Add this block to fetch macroeconomic data
+# Continue with existing code
+logging.info('Data downloaded from Yahoo Finance')
+logging.info(f'Data shape: {data.shape}')
+logging.info('First few rows of the data:')
+
+logging.info('Fetching macroeconomic data using FRED')
+start_date = '1993-01-29'
+end_date = today
+gdp = pdr.get_data_fred('GDP', start_date, end_date)
+unemployment = pdr.get_data_fred('UNRATE', start_date, end_date)
+
+# Add this block to preprocess macroeconomic data
+logging.info('Preprocessing macroeconomic data')
+gdp = gdp.resample('D').ffill()  # Fill missing values by forward filling
+unemployment = unemployment.resample('D').ffill()  # Fill missing values by forward filling
+
+# Add this block to merge macroeconomic data with existing data
+logging.info('Merging macroeconomic data with existing data')
+data = pd.merge(data, gdp, how='left', left_index=True, right_index=True)
+data = pd.merge(data, unemployment, how='left', left_index=True, right_index=True)
+
+# Add this block to engineer features from macroeconomic data
+logging.info('Engineering features from macroeconomic data')
+data['GDP Change'] = data['GDP'].pct_change()
+data['Unemployment Change'] = data['UNRATE'].pct_change()
+
+# Add this line to drop original macroeconomic columns
+data = data.drop(columns=['GDP', 'UNRATE'])
+
+# Add day of the week feature
+data['DayOfWeek'] = data.index.dayofweek
+
+# Add month of the year feature
+data['Month'] = data.index.month
+
+# Add is holiday feature
+cal = USFederalHolidayCalendar()
+holidays = cal.holidays(start=data.index.min(), end=data.index.max())
+data['IsHoliday'] = data.index.isin(holidays).astype(int)
+# Filter out non-business days
+data = data[data.index.dayofweek < 5]
+
+# Add technical indicators
+# Simple Moving Average (SMA)
+data['SMA'] = data['Close'].rolling(window=14).mean()
+
+# Exponential Moving Average (EMA)
+data['EMA'] = data['Close'].ewm(span=14).mean()
+
+# Relative Strength Index (RSI)
+delta = data['Close'].diff()
+up = delta.clip(lower=0)
+down = -1 * delta.clip(upper=0)
+ema_up = up.ewm(com=13, adjust=False).mean()
+ema_down = down.ewm(com=13, adjust=False).mean()
+rs = ema_up / ema_down
+data['RSI'] = 100 - (100 / (1 + rs))
+
+# Moving Average Convergence Divergence (MACD)
+exp1 = data['Close'].ewm(span=12, adjust=False).mean()
+exp2 = data['Close'].ewm(span=26, adjust=False).mean()
+macd = exp1 - exp2
+signal = macd.ewm(span=9, adjust=False).mean()
+data['MACD'] = macd - signal
+
+# Bollinger Bands
+data['MA20'] = data['Close'].rolling(window=20).mean()
+data['20dSTD'] = data['Close'].rolling(window=20).std()
+data['Upper'] = data['MA20'] + (data['20dSTD'] * 2)
+data['Lower'] = data['MA20'] - (data['20dSTD'] * 2)
+
+# Volume Weighted Average Price (VWAP)
+data['Cum_Daily_Returns'] = (data['Close'] / data['Close'].shift(1)) - 1
+data['Cumulative_Returns'] = (1 + data['Cum_Daily_Returns']).cumprod()
+data['VWAP'] = (data['Close'] * data['Volume']).cumsum() / data['Volume'].cumsum()
+data = data.dropna()
+
+# Select features
+logging.info('Selecting features')
+features = data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'SMA', 'EMA', 'RSI', 'MACD', 'Upper', 'Lower', 'Cumulative_Returns', 'VWAP', 'GDP Change', 'Unemployment Change']]
+logging.info('Features: ' + str(features.columns.tolist()))  # Log the order of features
+
+# Feature Engineering Validation
+logging.info('Feature Engineering Validation')
+logging.info(data[['SMA', 'EMA', 'RSI', 'MACD', 'Upper', 'Lower', 'Cumulative_Returns', 'VWAP']].head())
+logging.info(data[['SMA', 'EMA', 'RSI', 'MACD', 'Upper', 'Lower', 'Cumulative_Returns', 'VWAP']].tail())
+
+# Handle outliers
+Q1 = features.quantile(0.25)
+Q3 = features.quantile(0.75)
+IQR = Q3 - Q1
+features = features[~((features < (Q1 - 1.5 * IQR)) | (features > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+# Preprocess data
+logging.info('Preprocessing data')
+feature_scaler = StandardScaler()
+target_scaler = StandardScaler()
+
+# Split data into training and test sets
+logging.info('Splitting data into training and test sets')
+split = int(0.8 * len(features))
+train_features = features[:split]
+test_features = features[split:]
+
+# Fit the scaler on the training data and transform both training and test data
+scaled_train_features = feature_scaler.fit_transform(train_features)
+scaled_test_features = feature_scaler.transform(test_features)
+
+# Do the same for the target variable
+train_target = features[['Open']][:split]
+test_target = features[['Open']][split:]
+scaled_train_target = target_scaler.fit_transform(train_target)
+scaled_test_target = target_scaler.transform(test_target)
+
+# Create the dataset for training
+logging.info('Creating dataset for training')
+X_train, Y_train = [], []
+look_back = 22  # Define look_back here
+for i in range(look_back, len(train_features)):
+    X_train.append(scaled_train_features[i-look_back:i, :])
+    Y_train.append(scaled_train_target[i, 0])  # Use scaled_train_target here
+
+# Create the dataset for testing
+logging.info('Creating dataset for testing')
+X_test, Y_test = [], []
+for i in range(look_back, len(test_features)):
+    X_test.append(scaled_test_features[i-look_back:i, :])
+    Y_test.append(scaled_test_target[i, 0])  # Use scaled_test_target here
+
+# Convert lists to numpy arrays
+X_train, Y_train = np.array(X_train), np.array(Y_train)
+X_test, Y_test = np.array(X_test), np.array(Y_test)
+
+# Print the first few elements of Y_train and Y_test
+logging.info("First few elements of Y_train: " + str(Y_train[:5]))
+logging.info("First few elements of Y_test: " + str(Y_test[:5]))
+
+# Print the last few elements of Y_train and Y_test
+logging.info("Last few elements of Y_train: " + str(Y_train[-5:]))
+logging.info("Last few elements of Y_test: " + str(Y_test[-5:]))
+
+# Reshape data for LSTM
+logging.info('Reshaping data for LSTM')
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], train_features.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], test_features.shape[1]))
+
+# Get the minimum and maximum 'Close' prices
+min_close_price = data['Close'].min()
+max_close_price = data['Close'].max()
+
+# Manually compute the scaled value for the first 'Close' price
+first_close_price = data['Close'].iloc[0]
+scaled_first_close_price = (first_close_price - min_close_price) / (max_close_price - min_close_price)
+
+# Check if the manually computed scaled value matches the first value in scaled_train_target
+logging.info("Manually computed scaled value for the first 'Close' price: " + str(scaled_first_close_price))
+logging.info("First value in scaled_train_target: " + str(scaled_train_target[0][0]))
+
+
+# Function to create LSTM model
+def create_model(units=100, optimizer='adam', dropout_rate=0.0):
+    model = Sequential()
+    model.add(LSTM(units=units, return_sequences=True, input_shape=(look_back, features.shape[1]), kernel_regularizer=tf.keras.regularizers.L2(0.01)))
+    model.add(Dropout(dropout_rate))  # Add dropout layer
+    model.add(LSTM(units=units, return_sequences=True, kernel_regularizer=tf.keras.regularizers.L2(0.01)))
+    model.add(Dropout(dropout_rate))  # Add dropout layer
+    model.add(LSTM(units=units, return_sequences=False, kernel_regularizer=tf.keras.regularizers.L2(0.01)))
+    model.add(Dense(units=25))
+    model.add(Dense(units=1))
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    return model
+
+# Wrap Keras model with KerasRegressor
+model_params = {'units': 100, 'optimizer': 'adam', 'dropout_rate': 0.0}
+model = KerasRegressor(model=create_model, **model_params, verbose=0)
+
+# Define hyperparameters for RandomizedSearchCV
+param_dist = {
+    'units': [50, 100, 150, 200],  # More units can help model complexity
+    'batch_size': [16, 32, 64, 128],  # Larger batch sizes can speed up training
+    'epochs': [50, 100, 150],  # More epochs can lead to more stable models
+    'dropout_rate': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],  # Dropout can help prevent overfitting
+    'optimizer': ['Adam']  # Different optimizers can have different effects on training
+}
+
+# Use TimeSeriesSplit for cross-validation
+tscv = TimeSeriesSplit(n_splits=10)  # More splits can provide a more robust estimate of model performance
+
+# Define random_search before the try block
+random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist, cv=tscv, n_iter=20, n_jobs=-1)  # More iterations can explore the hyperparameter space more thoroughly
+
+# Check if hyperparameters file exists
+try:
+    with open('best_params.json', 'r') as f:
+        best_params = json.load(f)
+    logging.info(f'Loaded parameters: {best_params}')
+except FileNotFoundError:
+    # Perform RandomizedSearchCV
+    logging.info('Performing RandomizedSearchCV')
+    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist, cv=tscv, verbose=2, n_iter=10, n_jobs=-1)  # Use all cores
+
+    start_time = time.time()
+    random_search.fit(X_train, Y_train)
+    elapsed_time = time.time() - start_time
+    logging.info(f'RandomizedSearchCV completed. Elapsed time: {elapsed_time} seconds')
+
+    # Get the best parameters
+    best_params = random_search.best_params_
+    logging.info(f'Best parameters: {best_params}')
+
+    # Save the best parameters
+    with open('best_params.json', 'w') as f:
+        json.dump(best_params, f)
+    logging.info('Saved best parameters')
+
+# Define early stopping
+early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+# Define 5-fold cross validation
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+# Define a list to store the model objects of each fold
+models = []
+
+# Loop over each fold
+for train_index, val_index in kf.split(X_train):
+    # Create training and validation sets for this fold
+    X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
+    Y_train_fold, Y_val_fold = Y_train[train_index], Y_train[val_index]
+
+    # Create a new model for this fold
+    model = KerasRegressor(model=create_model, verbose=0, **best_params)
+
+    # Fit the model and store the model object
+    history = model.fit(X_train_fold, Y_train_fold, validation_data=(X_val_fold, Y_val_fold), verbose=1, callbacks=[early_stopping])
+
+    # Add the model object to the list
+    models.append(model)
+
+logging.info('Training completed')
+
+# Access the underlying Keras model and its history
+keras_model = model.model_
+history = keras_model.history.history
+
+# Generate predictions
+logging.info('Generating predictions')
+train_predict = model.predict(X_train)
+test_predict = model.predict(X_test)
+# Access the underlying Keras model
+keras_model = model.model_
+
+# Save the model
+model.model_.save('trained_model.h5')
+logging.info('Saved trained model to disk')
+
+# Predict prices
+logging.info('Predicting prices')
+
+# Reshape predictions into 2D arrays
+train_predict = train_predict.reshape(-1, 1)
+test_predict = test_predict.reshape(-1, 1)
+
+# Invert predictions using target_scaler
+train_predict = target_scaler.inverse_transform(train_predict)
+Y_train = target_scaler.inverse_transform(Y_train.reshape(-1, 1))
+test_predict = target_scaler.inverse_transform(test_predict)
+Y_test = target_scaler.inverse_transform(Y_test.reshape(-1, 1))
+
+# Calculate root mean squared error
+train_score = np.sqrt(mean_squared_error(Y_train, train_predict))
+logging.info(f'Train Score: {train_score} RMSE')
+test_score = np.sqrt(mean_squared_error(Y_test, test_predict))
+logging.info(f'Test Score: {test_score} RMSE')
+
+# Calculate mean absolute error
+train_score = mean_absolute_error(Y_train, train_predict)
+logging.info(f'Train Score: {train_score} MAE')
+test_score = mean_absolute_error(Y_test, test_predict)
+logging.info(f'Test Score: {test_score} MAE')
+
+# Connect to the database
+conn = db_operations.connect_to_db()
+cur = conn.cursor()
+
+# Create tables
+db_operations.create_tables(cur)
+
+# Insert data
+db_operations.insert_data(cur, history, Y_train, train_predict, test_predict)
+
+# Close the connection
+db_operations.close_connection(conn)
+
+logging.info('Script completed')
